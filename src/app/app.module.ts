cb14da0f83661edee6a3fef1a0f7d2272ff89108
Kanstantsin Kavalchuk <kanstantsin.kavalchuk@brz.gv.at>
2020-11-16 23:30:08 +0100

    Add heroes details screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ // make components, pipes etc. **from the current module** available to other directives in the current module
+    AppComponent,
+    HeroesComponent
   ],
-  imports: [
-    BrowserModule
+  imports: [ // makes the exported declarations of **some other module** available in the current module
+    BrowserModule // BrowserModule provides browser-specific services such as DOM rendering, sanitization, and location
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // make these services and values known to DI -> can be injected everywhere in this module
+  bootstrap: [AppComponent] // the root component that Angular creates and inserts into the index.html host web page
 })
 export class AppModule { }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+// public, private - has no effect on the generated JS. It is just a compile time tool that you can use to stop your TS code accessing things it shouldn't.
+// 'export' changes visibility in JS - TODO: what exactly is changed there?
+export class HeroesComponent implements OnInit { // has to always be exported -> app.module can import it -> 
+  // -> app.module can then add it to decalrations -> can be used by all other stuff inside app module 
+  
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+
+}
