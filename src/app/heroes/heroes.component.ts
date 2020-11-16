929c273271eadce66d2cd4e2166e762891aa22ef
Kanstantsin Kavalchuk <kanstantsin.kavalchuk@brz.gv.at>
2020-11-17 00:16:52 +0100

    Allow user select a hero by clicking

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,17 +12,17 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit { // has to always be exported -> app.module can import it -> 
   // -> app.module can then add it to decalrations -> can be used by all other stuff inside app module 
   
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero: Hero;
 
   heroes = HEROES;
 
-  constructor() { }
+  constructor() {  }
 
   ngOnInit() {
   }
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
 }
