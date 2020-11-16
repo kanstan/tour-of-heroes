3d912b85823c0679a3c03745b272b4948b68e6ae
Kanstantsin Kavalchuk <kanstantsin.kavalchuk@brz.gv.at>
2020-11-16 23:40:29 +0100

    Add edit field

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [ // make components, pipes etc. **from the current module** available to other directives in the current module
     AppComponent,
-    HeroesComponent
+    HeroesComponent // Every component must be declared in exactly one NgModule
   ],
   imports: [ // makes the exported declarations of **some other module** available in the current module
-    BrowserModule // BrowserModule provides browser-specific services such as DOM rendering, sanitization, and location
+    BrowserModule, // BrowserModule provides browser-specific services such as DOM rendering, sanitization, and location
+    FormsModule
   ],
   providers: [], // make these services and values known to DI -> can be injected everywhere in this module
   bootstrap: [AppComponent] // the root component that Angular creates and inserts into the index.html host web page
