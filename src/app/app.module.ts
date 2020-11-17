e58d937532d2c53d20e430d1a32e2ef7637b49ed
Kanstantsin Kavalchuk <kanstantsin.kavalchuk@brz.gv.at>
2020-11-17 09:39:03 +0100

    Extract hero details into new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @NgModule({
   declarations: [ // make components, pipes etc. **from the current module** available to other directives in the current module
     AppComponent,
-    HeroesComponent // Every component must be declared in exactly one NgModule
+    HeroesComponent,
+    HeroDetailComponent // Every component must be declared in exactly one NgModule
   ],
-  imports: [ // makes the exported declarations of **some other module** available in the current module
+  imports: [ // makes the exported declarations of **some other module** available in the current module, it is not enough to import on top
     BrowserModule, // BrowserModule provides browser-specific services such as DOM rendering, sanitization, and location
-    FormsModule
+    FormsModule // we need this e.g. to be able to use ngModel in heroes.component.html
   ],
   providers: [], // make these services and values known to DI -> can be injected everywhere in this module
   bootstrap: [AppComponent] // the root component that Angular creates and inserts into the index.html host web page
