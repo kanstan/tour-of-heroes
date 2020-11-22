5543434fab5b372c543414ebec3fe06b27e7403b
Kanstantsin Kavalchuk <kanstantsin.kavalchuk@brz.gv.at>
2020-11-22 19:36:57 +0100

    Add error handling

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,6 +4,7 @@ import { Hero } from './hero';
 import { MessageService } from './message.service';
 import { HEROES } from './mock-heroes';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 // @Injectable: Decorator that marks a class as available to be provided and injected as a dependency.
 // HeroService is the provider of its service
@@ -22,9 +23,13 @@ export class HeroService {
     private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
-    this.log('fetched heroes');
     // return of(HEROES); // comes from mock-heroes.ts
-    return this.http.get<Hero[]>(this.heroesUrl);
+
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetched heroes')), // only works if no error was thrown
+        catchError(this.handleError<Hero[]>('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
@@ -35,4 +40,24 @@ export class HeroService {
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
+
+  /**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+private handleError<T>(operation = 'operation', result?: T) {
+  return (error: any): Observable<T> => {
+
+    // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    this.log(`${operation} failed: ${error.message}`);
+
+    // Let the app keep running by returning an empty result.
+    return of(result as T);
+  };
+}
 }
