be9202dc5e0fdb1de389afc63b5bc546c38dfccf
Kanstantsin Kavalchuk <kanstantsin.kavalchuk@brz.gv.at>
2021-01-13 13:00:31 +0100

    Add comments egarding flatmap vs switchmap

--- src/app/hero-search/hero-search.component.ts
+++ src/app/hero-search/hero-search.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable, Subject} from 'rxjs';
 import {Hero} from '../hero';
-import {debounceTime, distinctUntilChanged, switchMap} from 'rxjs/operators';
+import {debounceTime, distinctUntilChanged, flatMap, map, switchMap} from 'rxjs/operators';
 import {HeroService} from '../hero.service';
 
 @Component({
@@ -24,8 +24,33 @@ export class HeroSearchComponent implements OnInit {
       // ignore new term if same as previous term
       distinctUntilChanged(),
 
-      // Map to observable, complete previous inner observable, emit values
-      // (cancels and discards previous search observables, returning only the latest search service observable)
+      // 1. map
+      // map the value 'term' to Observable returned by searchHeroes, return this Observable
+      // error: Type 'Observable<Observable<Hero[]>>' is not assignable to type 'Observable<Hero[]>'.
+      // searchHeroes returns Observable (inner O.) -> gets wrapped into another Observable by map() (outer Observable)
+      // -> returns Observable of Observable
+      //
+      // map(term => this.heroService.searchHeroes(term)),
+
+      // 2. flatMap / mergeMap
+      // this merges all the Observables inside into one Observable -> in our case inner and outer Observables are merged into one
+      // this fixes the 'Observable of Observable' problem
+      //
+      // flatMap(term => this.heroService.searchHeroes(term)),
+      // Problem with flatMap: response for `ABC` comes later and overwrites response for `ABCX`
+      // A1: Request for `ABC`
+      // A2: Response for `ABC` - e.g. takes longer time process than B1
+      // B1: Request for `ABCX`
+      // B2: Response for `ABCX`
+      // --A1----------A2-->
+      // ------B1--B2------>
+
+      // 3. switchMap
+      // Solution to flatMap problem: switchMap():
+      // 1. A1 search observable (inner observable) is made and subscribed ('inner subscription')
+      // 2. B1 search observable (inner observable) is made and subscribed
+      //   -> A1 observable gets immediately cancelled and subscription is discarded
+      // -> returns only the latest search service observable (= switchMap maintains only one inner subscription at a time!)
       switchMap((term: string) => this.heroService.searchHeroes(term)),
     );
   }
